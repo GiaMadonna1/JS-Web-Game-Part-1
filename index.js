d0a292c6eb9f5b0159f89bd78df284eee1ae2d88
Gia Madonna <giamadonna1@gmail.com>
2022-10-04 12:45:56 -0700

    Finished. Started the bonus

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+ 
+function newImg(url,left,bottom) {
+    let gameElements = document.createElement('img')    
+    gameElements.src = url
+    gameElements.style.position = 'fixed'
+    gameElements.style.left = left + 'px'
+    gameElements.style.bottom = bottom + 'px'
+    document.body.append(gameElements)
+    return gameElements
+     
+    }
+    function newItem(url,left,bottom) {
+    let itemUnlocked = newImg(url,left,bottom)
+    itemUnlocked.addEventListener('dblclick', function (){
+        itemUnlocked.remove()
+    })
+    }
+    function operationBackground(url,left,bottom) {
+        let newBackground = newImg(url,left,bottom)
+    }
+     
+    newItem('assets/sword.png', 500, 405)
+    newItem('assets/sheild.png', 165, 185)
+    newItem('assets/staff.png', 600, 100)
+    newImg('assets/green-character.gif', 100,100)
+    newImg('assets/pine-tree.png', 450,200)
+    newImg('assets/pillar.png', 350,100)
+    newImg('assets/tree.png', 200, 300)
+    newImg('assets/crate.png', 150, 200)
+    newImg('assets/well.png', 500, 425)
+    
